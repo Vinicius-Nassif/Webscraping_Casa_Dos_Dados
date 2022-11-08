ce7e61422a56d2ead5c70c7a2858033262b205d0
Vinicius-Nassif <111388699+Vinicius-Nassif@users.noreply.github.com>
2022-11-08 13:49:10 -0300

    Update data_to_mysql.py

--- data_to_mysql.py
+++ data_to_mysql.py
@@ -1,5 +1,5 @@
 import mysql.connector     # Disponibiliza função para conectar ao MySQL 
-import xlrd				   # Ferramenta para extrair informações de um .xls 
+import xlrd		   # Ferramenta para extrair informações de um .xls 
 import os                  # Funcionalidades dependentes do sistema operacional
 
 class Excel_to_Mysql():
@@ -19,9 +19,9 @@ class Excel_to_Mysql():
 		# Criação da tabela no servidor MySQL:
 		## Função importada da biblioteca mysql.connector para estabelecer conexão com MySQL.
 		self.conn = mysql.connector.connect(host = self.host,
-										user = self.my_db_user,
-										passwd = self.my_db_password, 
-										database = self.schema)
+						user = self.my_db_user,
+						passwd = self.my_db_password, 
+						database = self.schema)
 		self.cur = self.conn.cursor()
 
 		## Ferramenta para estabelecer dados de formatação e lacunas das tabelas.
@@ -32,9 +32,9 @@ class Excel_to_Mysql():
 	def conexao_mysql(self):
 		# Função importada da biblioteca mysql.connector para estabelecer conexão com MySQL.
 		self.conn = mysql.connector.connect(host = self.host,
-										user = self.my_db_user,
-										passwd = self.my_db_password, 
-										database = self.schema)
+						user = self.my_db_user,
+						passwd = self.my_db_password, 
+						database = self.schema)
 		self.cur = self.conn.cursor()
 		lista = list()
 		diretorio = ("C:\\Users\\ccece\\Documents\\Projetos AD\\"
